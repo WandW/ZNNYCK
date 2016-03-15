ce6c6ccce8364a72f6ccd0830f8c44d4b7bf225f
q974740928 <admin@4um.cn>
2016-03-15 21:25:36 +0800

    图片增加

--- ZNNY/src/com/example/znny/login.java
+++ ZNNY/src/com/example/znny/login.java
@@ -1,14 +1,16 @@
 package com.example.znny;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.Window;
 
 public class login extends Activity {
 	 @Override
 	    protected void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
-	        setContentView(R.layout.activity_main);
-
+	        setContentView(R.layout.login);
+	      
 	    }
 
 }
