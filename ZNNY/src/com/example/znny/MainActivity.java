2502e100e273cca5da75651ce3004e97acfe7e49
q974740928 <admin@4um.cn>
2016-03-15 20:43:42 +0800

    a

--- ZNNY/src/com/example/znny/MainActivity.java
+++ ZNNY/src/com/example/znny/MainActivity.java
@@ -10,19 +10,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
-        
-=======
-        jdsalkjdfsaklsfdsa
->>>>>>> a10c2d49f89b0e1a067d4d53f9d7f8aa1f191e00
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
 }
